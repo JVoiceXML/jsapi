a12c02ece0e293055d1138fb128a65207041e4e7
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2007-04-18 17:08:16 +0000

    Added header.

--- trunk/src/javax/speech/SpeechEvent.java
+++ trunk/src/javax/speech/SpeechEvent.java
@@ -1,8 +1,33 @@
+/*
+ * File:    $HeadURL: https://svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/Application.java$
+ * Version: $LastChangedRevision: 249 $
+ * Date:    $LastChangedDate $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JSAPI - An independent reference implementation of JSR 113.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package javax.speech;
 
 import java.util.EventObject;
 
-@SuppressWarnings("serial")
 public class SpeechEvent extends EventObject {
     private final int id;
     
@@ -21,12 +46,23 @@ public class SpeechEvent extends EventObject {
     }
     
     public String paramString() {
-        // TODO: check creation of the param string.
-        return Integer.toString(id);
+        StringBuffer str = new StringBuffer();
+        
+        str.append(Integer.toString(id));
+        str.append(",");
+        str.append(getSource());
+        
+        return str.toString();
     }
     
     public String toString() {
-        // TODO: check creation of toString.
-        return super.toString();
+        StringBuffer str = new StringBuffer();
+        
+        str.append(getClass().getName());
+        str.append("[");
+        str.append(paramString());
+        str.append("]");
+        
+        return str.toString();
     }
 }
