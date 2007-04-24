3d850c92f51e39cda34411cb59eeae3e970ce7b5
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2007-04-24 07:21:20 +0000

    Added PhoneInfoTest and ResultEventTest.

---
+++ unittests/javax/speech/synthesis/PhoneInfoTest.java
@@ -0,0 +1,62 @@
+/*
+ * File:    $HeadURL: $
+ * Version: $LastChangedRevision: $
+ * Date:    $LastChangedDate: $
+ * Author:  $LastChangedBy: $
+ *
+ * JSAPI - An independent reference implementation of JSR 113.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package javax.speech.synthesis;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for {@link jjavax.speech.synthesis.PhoneInfo}.
+ * 
+ * @author Dirk Schnelle
+ */
+public class PhoneInfoTest extends TestCase {
+
+    /**
+     * Test method for {@link javax.speech.synthesis.PhoneInfo#getDuration()}.
+     */
+    public void testGetDuration() {
+        final PhoneInfo info = new PhoneInfo("phoneme", 45);
+        assertEquals(45, info.getDuration());
+
+        final PhoneInfo info2 = new PhoneInfo("phoneme2", 
+                PhoneInfo.UNKNOWN_DURATION);
+        assertEquals(PhoneInfo.UNKNOWN_DURATION, info2.getDuration());
+    }
+
+    /**
+     * Test method for {@link javax.speech.synthesis.PhoneInfo#getPhoneme()}.
+     */
+    public void testGetPhoneme() {
+        final PhoneInfo info = new PhoneInfo("phoneme", 45);
+        assertEquals("phoneme", info.getPhoneme());
+
+        final PhoneInfo info2 = new PhoneInfo("phoneme2", 
+                PhoneInfo.UNKNOWN_DURATION);
+        assertEquals("phoneme2", info2.getPhoneme());
+    }
+
+}
---
+++ unittests/javax/speech/test/recognizer/TestResult.java
@@ -0,0 +1,94 @@
+/*
+ * File:    $HeadURL: $
+ * Version: $LastChangedRevision: $
+ * Date:    $LastChangedDate: $
+ * Author:  $LastChangedBy: $
+ *
+ * JSAPI - An independent reference implementation of JSR 113.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package javax.speech.test.recognizer;
+
+import javax.speech.recognition.Grammar;
+import javax.speech.recognition.Result;
+import javax.speech.recognition.ResultListener;
+import javax.speech.recognition.ResultToken;
+
+/**
+ * Result for test purposes.
+ * 
+ * @author Dirk Schnelle
+ */
+public class TestResult implements Result {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addResultListener(ResultListener listener) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ResultToken getBestToken(int tokNum) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ResultToken[] getBestTokens() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Grammar getGrammar() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getNumTokens() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getResultState() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ResultToken[] getUnfinalizedTokens() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeResultListener(ResultListener listener) {
+    }
+}
