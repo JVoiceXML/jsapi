c1db123d7e5136d65209ef43773c0eda31166857
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2007-05-16 07:21:57 +0000

    Added tests for RuleToken and RuleTag.

---
+++ unittests/javax/speech/recognition/RuleComponentTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package javax.speech.recognition;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for {@link javax.speech.recognition.RuleComponent}.
+ * 
+ * @author Dirk Schnelle
+ */
+public class RuleComponentTest extends TestCase {
+
+	/**
+	 * Test method for {@link javax.speech.recognition.RuleComponent#toString()}.
+	 */
+	public void testToString() {
+		final RuleComponent component = new RuleComponent();
+		assertNull(component.toString());
+	}
+
+}
