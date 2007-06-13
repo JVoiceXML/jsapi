506ff6a873e05a2aea217b73df31b16f6f9154c0
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2007-06-13 18:44:34 +0000

    Completed tests for AudioSegment and EngineManager.

--- trunk/src/javax/speech/AudioSegment.java
+++ trunk/src/javax/speech/AudioSegment.java
@@ -29,39 +29,43 @@ package javax.speech;
 import java.io.ByteArrayInputStream;
 
 public class AudioSegment {
-    private ByteArrayInputStream stream;
-    
-    private String locator;
-    
-    private String markupText;
-    
-    public AudioSegment(ByteArrayInputStream stream, String locator, 
-            String markupText) {
-       this.stream = stream;
-       this.locator = locator;
-       this.markupText = markupText;
-    }
-    
-    public AudioSegment(String locator, String markupText) {
-        this.locator = locator;
-        this.markupText = markupText;
-    }
-    
-    public String getLocator() {
-        return locator;
-    }
+	private ByteArrayInputStream stream;
 
-    public String getMarkupText() {
-        return markupText;
-    }
+	private String locator;
 
-    public ByteArrayInputStream getInputStream() {
-        // TODO: check system property javax.speech.supports.audio.capture
-        return stream;
-    }
+	private String markupText;
 
-    public boolean isGettable() {
-        // TODO: check system property javax.speech.supports.audio.capture
-        return stream != null;
-    }
+	public AudioSegment(ByteArrayInputStream stream, String locator,
+			String markupText) {
+		this.stream = stream;
+		this.locator = locator;
+		this.markupText = markupText;
+		// TODO: How can we check if the locator is supported?
+	}
+
+	public AudioSegment(String locator, String markupText) {
+		this.locator = locator;
+		this.markupText = markupText;
+	}
+
+	public String getLocator() {
+		return locator;
+	}
+
+	public String getMarkupText() {
+		return markupText;
+	}
+
+	public ByteArrayInputStream getInputStream() {
+		if (!isGettable()) {
+			throw new SecurityException(
+					"The platform does not allow to access the input stream!");
+		}
+		
+		return stream;
+	}
+
+	public boolean isGettable() {
+		return Boolean.getBoolean("javax.speech.supports.audio.capture");
+	}
 }
