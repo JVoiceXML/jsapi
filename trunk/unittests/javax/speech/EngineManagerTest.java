254ebca0a15936e3017e9b66100a539d5ad90544
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2007-06-13 18:46:12 +0000

    Completed tests for AudioSegment and EngineManager.

--- trunk/unittests/javax/speech/EngineManagerTest.java
+++ trunk/unittests/javax/speech/EngineManagerTest.java
@@ -3,6 +3,12 @@
  */
 package javax.speech;
 
+import javax.speech.recognition.RecognizerMode;
+import javax.speech.spi.EngineListFactory;
+import javax.speech.synthesis.SynthesizerMode;
+import javax.speech.test.TestRecognizerEngineListFactory;
+import javax.speech.test.TestSpeechEventExecutor;
+
 import junit.framework.TestCase;
 
 /**
@@ -12,24 +18,63 @@ import junit.framework.TestCase;
  */
 public class EngineManagerTest extends TestCase {
 	/**
-	 * Test method for {@link javax.speech.EngineManager#availableEngines(javax.speech.EngineMode)}.
+	 * Test method for
+	 * {@link javax.speech.EngineManager#availableEngines(javax.speech.EngineMode)}.
 	 */
 	public void testAvailableEngines() {
-		fail("Not yet implemented");
+		final EngineMode require1 = null;
+		final EngineList engines1 = EngineManager.availableEngines(require1);
+		assertEquals(1, engines1.size());
+
+		final EngineMode require2 = new SynthesizerMode();
+		final EngineList engines2 = EngineManager.availableEngines(require2);
+		assertEquals(1, engines2.size());
+
+		final EngineMode require3 = new RecognizerMode();
+		final EngineList engines3 = EngineManager.availableEngines(require3);
+		assertEquals(0, engines3.size());
 	}
 
 	/**
-	 * Test method for {@link javax.speech.EngineManager#createEngine(javax.speech.EngineMode)}.
+	 * Test method for
+	 * {@link javax.speech.EngineManager#createEngine(javax.speech.EngineMode)}.
 	 */
-	public void testCreateEngine() {
-		fail("Not yet implemented");
+	public void testCreateEngine() throws Exception {
+		final EngineMode require1 = new SynthesizerMode();
+		final Engine engine1 = EngineManager.createEngine(require1);
+		assertNotNull(engine1);
+
+		final EngineMode require2 = new RecognizerMode();
+		final Engine engine2 = EngineManager.createEngine(require2);
+		assertNull(engine2);
+
+		final EngineMode require3 = null;
+		Exception failure = null;
+		try {
+			final Engine engine3 = EngineManager.createEngine(require3);
+		} catch (IllegalArgumentException e) {
+			failure = e;
+		}
+		assertNotNull(failure);
 	}
 
 	/**
-	 * Test method for {@link javax.speech.EngineManager#getSpeechEventExecutor()}.
+	 * Test method for
+	 * {@link javax.speech.EngineManager#getSpeechEventExecutor()}.
 	 */
 	public void testGetSpeechEventExecutor() {
-		fail("Not yet implemented");
+		assertNull(EngineManager.getSpeechEventExecutor());
+	}
+
+	/**
+	 * Test method for
+	 * {@link javax.speech.EngineManager#setSpeechEventExecutor(javax.speech.SpeechEventExecutor)}.
+	 */
+	public void testSetSpeechEventExecutor() {
+		assertNull(EngineManager.getSpeechEventExecutor());
+		final SpeechEventExecutor executor = new TestSpeechEventExecutor();
+		EngineManager.setSpeechEventExecutor(executor);
+		assertEquals(executor, EngineManager.getSpeechEventExecutor());
 	}
 
 	/**
@@ -40,17 +85,25 @@ public class EngineManagerTest extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link javax.speech.EngineManager#registerEngineListFactory(java.lang.String)}.
+	 * Test method for
+	 * {@link javax.speech.EngineManager#registerEngineListFactory(java.lang.String)}.
 	 */
-	public void testRegisterEngineListFactory() {
-		fail("Not yet implemented");
-	}
+	public void testRegisterEngineListFactory() throws Exception {
+		final EngineList engines1 = EngineManager.availableEngines(null);
+		assertEquals(1, engines1.size());
 
-	/**
-	 * Test method for {@link javax.speech.EngineManager#setSpeechEventExecutor(javax.speech.SpeechEventExecutor)}.
-	 */
-	public void testSetSpeechEventExecutor() {
-		fail("Not yet implemented");
-	}
+		EngineManager
+				.registerEngineListFactory(TestRecognizerEngineListFactory.class
+						.getCanonicalName());
 
+		final EngineList engines2 = EngineManager.availableEngines(null);
+		assertEquals(2, engines2.size());
+
+		EngineManager
+				.registerEngineListFactory(TestRecognizerEngineListFactory.class
+						.getCanonicalName());
+
+		final EngineList engines3 = EngineManager.availableEngines(null);
+		assertEquals(2, engines3.size());
+	}
 }
