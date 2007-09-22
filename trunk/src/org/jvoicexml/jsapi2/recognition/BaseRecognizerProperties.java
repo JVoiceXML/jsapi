b2c4c0b8c3896688bd728e21c44c3fab69de99d4
lyncher <lyncher@8d230bc6-492e-0410-ad60-ad825706fda1>
2007-09-22 02:05:42 +0000

    Storing values and firing change events.

--- trunk/src/org/jvoicexml/jsapi2/recognition/BaseRecognizerProperties.java
+++ trunk/src/org/jvoicexml/jsapi2/recognition/BaseRecognizerProperties.java
@@ -16,265 +16,295 @@ import javax.speech.recognition.Recognizer;
  * @author Renato Cassaca
  * @version 1.0
  */
-public class BaseRecognizerProperties extends BaseEngineProperties implements RecognizerProperties {
+public class BaseRecognizerProperties extends BaseEngineProperties implements
+        RecognizerProperties {
+
+    protected int adaptation;
+    protected int completeTimeout;
+    protected int confidenceThreshold;
+    protected int endpointStyle;
+    protected int incompleteTimeout;
+    protected int numResultAlternatives;
+    protected int sensitivity;
+    protected int speedVsAccuracy;
+    protected boolean resultAudioProvided;
+    protected boolean trainingProvided;
 
     public BaseRecognizerProperties(Recognizer recognizer) {
         super(recognizer);
+        reset();
     }
 
     /**
      * getAdaptation
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getAdaptation() {
-        return 0;
-    }
-
-    /**
-     * getBase
-     *
-     * @return String
-     * @todo Implement this javax.speech.EngineProperties method
-     */
-    public String getBase() {
-        return "";
+        return adaptation;
     }
 
     /**
      * getCompleteTimeout
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getCompleteTimeout() {
-        return 0;
+        return completeTimeout;
     }
 
     /**
      * getConfidenceThreshold
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getConfidenceThreshold() {
-        return 0;
+        return confidenceThreshold;
     }
 
     /**
      * getEndpointStyle
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getEndpointStyle() {
-        return 0;
+        return endpointStyle;
     }
 
     /**
      * getIncompleteTimeout
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getIncompleteTimeout() {
-        return 0;
+        return incompleteTimeout;
     }
 
     /**
      * getNumResultAlternatives
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getNumResultAlternatives() {
-        return 0;
-    }
-
-    /**
-     * getPriority
-     *
-     * @return int
-     * @todo Implement this javax.speech.EngineProperties method
-     */
-    public int getPriority() {
-        return 0;
+        return numResultAlternatives;
     }
 
     /**
      * getSensitivity
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getSensitivity() {
-        return 0;
+        return sensitivity;
     }
 
     /**
      * getSpeedVsAccuracy
      *
      * @return int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public int getSpeedVsAccuracy() {
-        return 0;
+        return speedVsAccuracy;
     }
 
     /**
      * isResultAudioProvided
      *
      * @return boolean
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public boolean isResultAudioProvided() {
-        return false;
+        return resultAudioProvided;
     }
 
     /**
      * isTrainingProvided
      *
      * @return boolean
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public boolean isTrainingProvided() {
-        return false;
+        return trainingProvided;
     }
 
     /**
      * reset
      *
-     * @todo Implement this javax.speech.EngineProperties method
      */
     public void reset() {
+        setAdaptation(ADAPT_PAUSED | ADAPT_RESUMED);
+        setCompleteTimeout(500);
+        setConfidenceThreshold((MAX_CONFIDENCE - MIN_CONFIDENCE) / 4);
+        setEndpointStyle(ENDPOINT_SPEECH_DETECTION);
+        setIncompleteTimeout(1000);
+        setNumResultAlternatives(1);
+        setSensitivity(NORM_ACCURACY);
+        setSpeedVsAccuracy(NORM_ACCURACY);
+        setResultAudioProvided(false);
+        setTrainingProvided(false);
+
+        super.reset();
     }
 
     /**
      * setAdaptation
      *
      * @param adapt int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setAdaptation(int adapt) {
-    }
-
-    /**
-     * setBase
-     *
-     * @param uri String
-     * @todo Implement this javax.speech.EngineProperties method
-     */
-    public void setBase(String uri) {
+    public void setAdaptation(int adapt) throws IllegalArgumentException {
+        postPropertyChangeEvent("adaptation", new Integer(this.adaptation),
+                                new Integer(adapt));
+        this.adaptation = adapt;
     }
 
     /**
      * setCompleteTimeout
      *
      * @param timeout int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setCompleteTimeout(int timeout) {
+    public void setCompleteTimeout(int timeout) throws IllegalArgumentException {
+        if (timeout < 0)
+            throw new IllegalArgumentException(
+                    "Invalid completeTimeout: " + timeout);
+
+        postPropertyChangeEvent("completeTimeout",
+                                new Integer(this.completeTimeout),
+                                new Integer(timeout));
+        this.completeTimeout = timeout;
     }
 
     /**
      * setConfidenceThreshold
      *
      * @param confidenceThreshold int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setConfidenceThreshold(int confidenceThreshold) {
+    public void setConfidenceThreshold(int threshold) throws
+            IllegalArgumentException {
+        if ((confidenceThreshold > MAX_CONFIDENCE) ||
+            (confidenceThreshold < MIN_CONFIDENCE))
+            throw new IllegalArgumentException("Invalid confidenceThreshold: " +
+                                               threshold);
+
+        postPropertyChangeEvent("confidenceThreshold",
+                                new Integer(this.confidenceThreshold),
+                                new Integer(threshold));
+        this.confidenceThreshold = threshold;
     }
 
     /**
      * setEndpointStyle
      *
      * @param endpointStyle int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setEndpointStyle(int endpointStyle) {
+    public void setEndpointStyle(int style) throws
+            IllegalArgumentException {
+        if ((style != ENDPOINT_SPEECH_DETECTION) &&
+            (style != ENDPOINT_PUSH_TO_START) &&
+            (style != ENDPOINT_PUSH_TO_TALK)) {
+            throw new IllegalArgumentException("Invalid endpointStyle: " +
+                                               style);
+        }
+        postPropertyChangeEvent("confidenceThreshold",
+                                new Integer(this.endpointStyle),
+                                new Integer(style));
+        this.endpointStyle = style;
     }
 
     /**
      * setIncompleteTimeout
      *
      * @param timeout int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setIncompleteTimeout(int timeout) {
+    public void setIncompleteTimeout(int timeout) throws
+            IllegalArgumentException {
+        if (timeout < 0)
+            throw new IllegalArgumentException("Invalid incompleteTimeout: " +
+                                               timeout);
+        postPropertyChangeEvent("incompleteTimeout",
+                                new Integer(this.incompleteTimeout),
+                                new Integer(timeout));
+        this.incompleteTimeout = timeout;
     }
 
     /**
      * setNumResultAlternatives
      *
      * @param num int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setNumResultAlternatives(int num) {
-    }
-
-    /**
-     * setPriority
-     *
-     * @param priority int
-     * @todo Implement this javax.speech.EngineProperties method
-     */
-    public void setPriority(int priority) {
-    }
-
-    /**
-     * setResultAudioProvided
-     *
-     * @param audioProvided boolean
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
-     */
-    public void setResultAudioProvided(boolean audioProvided) {
+    public void setNumResultAlternatives(int num) throws
+            IllegalArgumentException {
+        postPropertyChangeEvent("numResultAlternatives",
+                                new Integer(this.numResultAlternatives),
+                                new Integer(num));
+        this.numResultAlternatives = num;
     }
 
     /**
      * setSensitivity
      *
      * @param sensitivity int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setSensitivity(int sensitivity) {
+    public void setSensitivity(int sensitivity) throws IllegalArgumentException {
+        if ((sensitivity > MAX_CONFIDENCE) ||
+            (sensitivity < MIN_CONFIDENCE))
+            throw new IllegalArgumentException("Invalid sensitivity: " +
+                                               sensitivity);
+
+        postPropertyChangeEvent("sensitivity",
+                                new Integer(this.sensitivity),
+                                new Integer(sensitivity));
+        this.sensitivity = sensitivity;
     }
+
 
     /**
      * setSpeedVsAccuracy
      *
      * @param speedVsAccuracy int
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
+     * @throws IllegalArgumentException
      */
-    public void setSpeedVsAccuracy(int speedVsAccuracy) {
+    public void setSpeedVsAccuracy(int speedVsAccuracy) throws
+            IllegalArgumentException {
+        if ((speedVsAccuracy != MAX_ACCURACY) &&
+            (speedVsAccuracy != NORM_ACCURACY) &&
+            (speedVsAccuracy != MAX_ACCURACY)) {
+            throw new IllegalArgumentException("Invalid speedVsAccuracy: " +
+                                               speedVsAccuracy);
+        }
+        postPropertyChangeEvent("speedVsAccuracy",
+                                new Integer(this.speedVsAccuracy),
+                                new Integer(speedVsAccuracy));
+        this.speedVsAccuracy = speedVsAccuracy;
+    }
+
+    /**
+     * setResultAudioProvided
+     *
+     * @param audioProvided boolean
+     */
+    public void setResultAudioProvided(boolean audioProvided) {
+        postPropertyChangeEvent("resultAudioProvided",
+                                new Boolean(this.resultAudioProvided),
+                                new Boolean(audioProvided));
+        this.resultAudioProvided = audioProvided;
     }
 
     /**
      * setTrainingProvided
      *
      * @param trainingProvided boolean
-     * @todo Implement this javax.speech.recognition.RecognizerProperties
-     *   method
      */
     public void setTrainingProvided(boolean trainingProvided) {
+        postPropertyChangeEvent("trainingProvided",
+                                new Boolean(this.trainingProvided),
+                                new Boolean(trainingProvided));
+        this.trainingProvided = trainingProvided;
     }
+
+
 }
