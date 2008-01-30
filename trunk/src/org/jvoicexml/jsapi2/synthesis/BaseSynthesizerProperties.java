b1f55b057cdf1c27e90bb1fa9728d444c9c0e86d
davidjrodrigues <davidjrodrigues@8d230bc6-492e-0410-ad60-ad825706fda1>
2008-01-30 19:13:35 +0000

    Improve SynthesizerProperties manager; send Speakable events to eventListener of speak methods

--- trunk/src/org/jvoicexml/jsapi2/synthesis/BaseSynthesizerProperties.java
+++ trunk/src/org/jvoicexml/jsapi2/synthesis/BaseSynthesizerProperties.java
@@ -8,6 +8,9 @@ import javax.speech.synthesis.Synthesizer;
 import java.util.Vector;
 import javax.speech.synthesis.SynthesizerMode;
 import javax.speech.EngineMode;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.concurrent.Semaphore;
 
 /**
  * <p>Title: JSAPI 2.0</p>
@@ -23,16 +26,18 @@ import javax.speech.EngineMode;
  */
 public class BaseSynthesizerProperties extends BaseEngineProperties implements SynthesizerProperties {
 
-    private int interruptibility;
-    private int pitch;
-    private int pitchRange;
-    private int speakingRate;
-    private Voice voice;
-    private int volume;
+    private HashMap<String,Object> uncommitedProperties;
+    private HashMap<String,Object> properties;
+    private Semaphore propertiesSemaphore;
+    private Semaphore uncommitedSemaphore;
 
 
     public BaseSynthesizerProperties(Synthesizer synthesizer) {
         super(synthesizer);
+        properties = new HashMap<String,Object>();
+        uncommitedProperties = new HashMap<String,Object>();
+        propertiesSemaphore = new Semaphore(1);
+        uncommitedSemaphore = new Semaphore(1);
         reset();
     }
 
@@ -42,7 +47,12 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return int
      */
     public int getInterruptibility() {
-        return interruptibility;
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        return ((Integer)(properties.get("interruptibility"))).intValue();
     }
 
     /**
@@ -51,6 +61,14 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return int
      */
     public int getPitch() {
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        int pitch = ((Integer)(properties.get("pitch"))).intValue();
+        propertiesSemaphore.release();
         return pitch;
     }
 
@@ -60,7 +78,15 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return int
      */
     public int getPitchRange() {
-        return pitchRange;
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+       int pitchRange = ((Integer)(properties.get("pitchRange"))).intValue();
+       propertiesSemaphore.release();
+       return pitchRange;
     }
 
     /**
@@ -69,6 +95,14 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return int
      */
     public int getSpeakingRate() {
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        int speakingRate = ((Integer)(properties.get("speakingRate"))).intValue();
+        propertiesSemaphore.release();
         return speakingRate;
     }
 
@@ -78,6 +112,14 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return Voice
      */
     public Voice getVoice() {
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        Voice voice =  (Voice)(properties.get("voice"));
+        propertiesSemaphore.release();
         return voice;
     }
 
@@ -87,6 +129,14 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @return int
      */
     public int getVolume() {
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        int volume = ((Integer)(properties.get("volume"))).intValue();
+        propertiesSemaphore.release();
         return volume;
     }
 
@@ -95,21 +145,27 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      *
      */
     public void reset() {
-        setInterruptibility(OBJECT_LEVEL);
-        setPitch(160);
-        setPitchRange((int)(160 * 0.60));
-        setSpeakingRate(DEFAULT_RATE);
-        setVolume(MEDIUM_VOLUME);
+        try {
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
 
+        properties.put("interruptibility", OBJECT_LEVEL);
+        properties.put("picth", 160);
+        properties.put("pitchRange", (int)(160 * 0.60));
+        properties.put("speakingRate", DEFAULT_RATE);
+        properties.put("volume", MEDIUM_VOLUME);
         //Set default voice
         Voice[] voices = ((SynthesizerMode)((Synthesizer)engine).getEngineMode()).getVoices();
         if ((voices != null) && (voices.length > 0)) {
-            setVoice(voices[0]);
+            properties.put("voice", voices[0]);
         }
         else {
-            setVoice(null);
+            properties.put("voice", null);
         }
 
+        propertiesSemaphore.release();
         super.reset();
     }
 
@@ -125,9 +181,13 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      *
      */
     public void setInterruptibility(int level) {
-        postPropertyChangeEvent("interruptibility", new Integer(interruptibility),
-                                new Integer(level));
-        interruptibility = level;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("interruptibility", new Integer(level));
+        uncommitedSemaphore.release();
     }
 
     /**
@@ -136,8 +196,13 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @param hertz int
      */
     public void setPitch(int hertz) {
-        postPropertyChangeEvent("pitch", new Integer(pitch), new Integer(hertz));
-        pitch = hertz;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("pitch", new Integer(hertz));
+        uncommitedSemaphore.release();
     }
 
     /**
@@ -146,8 +211,13 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @param hertz int
      */
     public void setPitchRange(int hertz) {
-        postPropertyChangeEvent("pitchRange", new Integer(pitchRange), new Integer(hertz));
-        pitchRange = hertz;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("pitchRange", new Integer(hertz));
+        uncommitedSemaphore.release();
     }
 
     /**
@@ -156,8 +226,13 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @param wpm int
      */
     public void setSpeakingRate(int wpm) {
-        postPropertyChangeEvent("speakingRate", new Integer(speakingRate), new Integer(wpm));
-        speakingRate = wpm;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("speakingRate", new Integer(wpm));
+        uncommitedSemaphore.release();
     }
 
     /**
@@ -166,8 +241,13 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @param voice Voice
      */
     public void setVoice(Voice voice) {
-        postPropertyChangeEvent("voice", this.voice, voice);
-        this.voice = voice;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("voice", voice);
+        uncommitedSemaphore.release();
     }
 
     /**
@@ -176,7 +256,39 @@ public class BaseSynthesizerProperties extends BaseEngineProperties implements S
      * @param volume int
      */
     public void setVolume(int volume) {
-        postPropertyChangeEvent("volume", new Integer(this.volume), new Integer(volume));
-        this.volume = volume;
+        try {
+            uncommitedSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        uncommitedProperties.put("volume", volume);
+        uncommitedSemaphore.release();
     }
+
+
+    /**
+     * commitPropertiesChanges
+     */
+    public void commitPropertiesChanges(){
+        try {
+            uncommitedSemaphore.acquire();
+            propertiesSemaphore.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        Iterator it = uncommitedProperties.keySet().iterator();
+        while (it.hasNext()){
+            String propertieName = (String)it.next();
+            Object oldValue = properties.get(propertieName);
+            Object newValue = uncommitedProperties.get(propertieName);
+            properties.put(propertieName, newValue);
+            postPropertyChangeEvent(propertieName, oldValue, newValue);
+            uncommitedProperties.remove(propertieName);
+        }
+
+        uncommitedSemaphore.release();
+        propertiesSemaphore.release();
+    }
+
 }
