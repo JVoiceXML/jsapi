177a13352470a305a518f9beb6b867db589e16bf
lyncher <lyncher@8d230bc6-492e-0410-ad60-ad825706fda1>
2008-02-14 06:50:27 +0000

    Deal with audio conversions.

--- trunk/src/org/jvoicexml/jsapi2/BaseAudioManager.java
+++ trunk/src/org/jvoicexml/jsapi2/BaseAudioManager.java
@@ -45,20 +45,35 @@
  */
 package org.jvoicexml.jsapi2;
 
+
 import javax.speech.AudioManager;
 import javax.speech.AudioListener;
-import java.util.Vector;
 import javax.speech.AudioException;
 import javax.speech.EngineStateException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import javax.speech.AudioEvent;
-import java.util.Enumeration;
 import javax.speech.recognition.Recognizer;
 import javax.speech.synthesis.Synthesizer;
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -71,7 +86,7 @@ public class BaseAudioManager implements AudioManager {
      * List of <code>AudioListeners</code> registered for
      * <code>AudioEvents</code> on this object.
      */
-    protected Vector audioListeners;
+    protected List<AudioListener> audioListeners;
 
     protected int audioMask;
 
@@ -83,16 +98,31 @@ public class BaseAudioManager implements AudioManager {
 
     protected InputStream inputStream;
 
+    protected AudioInputStream ais;
+
+    /**
+     *  Audio format of the audio nativelly produced
+     * by the engine
+     */
+    protected AudioFormat engineAudioFormat;
+
     /**
      * Class constructor.
      */
     public BaseAudioManager(BaseEngine engine) {
         this.engine = engine;
-        audioListeners = new Vector();
+        audioListeners = new ArrayList<AudioListener>();
         audioMask = AudioEvent.DEFAULT_MASK;
         inputStream = null;
         outputStream = null;
         mediaLocator = "";
+        engineAudioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                                            16000,
+                                            16,
+                                            1,
+                                            2,
+                                            16000,
+                                            false);
     }
 
     /**
@@ -102,8 +132,10 @@ public class BaseAudioManager implements AudioManager {
      * @param listener the listener to add
      */
     public void addAudioListener(AudioListener listener) {
-        if (!audioListeners.contains(listener)) {
-            audioListeners.addElement(listener);
+        synchronized (audioListeners) {
+            if (!audioListeners.contains(listener)) {
+                audioListeners.add(listener);
+            }
         }
     }
 
@@ -114,7 +146,9 @@ public class BaseAudioManager implements AudioManager {
      * @param listener the listener to remove
      */
     public void removeAudioListener(AudioListener listener) {
-        audioListeners.removeElement(listener);
+        synchronized (audioListeners) {
+            audioListeners.remove(listener);
+        }
     }
 
     public int getAudioMask() {
@@ -160,7 +194,8 @@ public class BaseAudioManager implements AudioManager {
                 throw new AudioException("Cannot get OutputStream from URL: " +
                                          ex.getMessage());
             }
-            outputStream = os;
+            outputStream = getConvertedStream(os, engineAudioFormat,
+                                              getAudioFormat(url));
         } else {
             InputStream is = null;
             try {
@@ -169,7 +204,10 @@ public class BaseAudioManager implements AudioManager {
                 throw new AudioException("Cannot get InputStream from URL: " +
                                          ex.getMessage());
             }
-            inputStream = is;
+
+            //Configure audio conversions
+            inputStream = getConvertedStream(is, engineAudioFormat,
+                                             getAudioFormat(url));
         }
 
         postAudioEvent(AudioEvent.AUDIO_STARTED, AudioEvent.AUDIO_LEVEL_MIN);
@@ -182,6 +220,22 @@ public class BaseAudioManager implements AudioManager {
         if (System.getProperty("javax.speech.supports.audio.management") == null) {
             throw new SecurityException(
                     "AudioManager has no permission to access audio resources");
+        }
+
+        //Release IO
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
 
         postAudioEvent(AudioEvent.AUDIO_STOPPED, AudioEvent.AUDIO_LEVEL_MIN);
@@ -317,10 +371,10 @@ public class BaseAudioManager implements AudioManager {
     }
 
     public void fireAudioEvent(AudioEvent event) {
-        Enumeration listeners = audioListeners.elements();
-        while (listeners.hasMoreElements()) {
-            AudioListener al = (AudioListener) listeners.nextElement();
-            ((AudioListener) al).audioUpdate(event);
+        synchronized (audioListeners) {
+            for (AudioListener listener: audioListeners) {
+                listener.audioUpdate(event);
+            }
         }
     }
 
@@ -330,6 +384,295 @@ public class BaseAudioManager implements AudioManager {
 
     public InputStream getInputStream() {
         return inputStream;
+    }
+
+    /**
+     * Given URI parameters, constructs an AudioFormat
+     *
+     * @return AudioFormat
+     */
+    private AudioFormat getAudioFormat(URL mediaLocator) {
+        //Get matching URI to extract query parameters
+        URI uri = null;
+        try {
+            uri = mediaLocator.toURI();
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
+            //Continue and give back a default AudioFormat
+        }
+
+        //Initialize parameters
+        HashMap<String, String> parameters = new HashMap<String, String>();
+        if (uri.getQuery() != null) {
+            String[] parametersString = uri.getQuery().split("\\&");
+            for (String part : parametersString) {
+                String[] queryElement = part.split("\\=");
+                parameters.put(queryElement[0], queryElement[1]);
+            }
+        }
+
+        //Default values for AudioFormat parameters
+        AudioFormat.Encoding encoding = AudioFormat.Encoding.PCM_SIGNED;
+        float sampleRate = 16000;
+        int bits = 16;
+        int channels = 1;
+        boolean endian = false;
+        boolean signed = true;
+
+        //Change default values as specified
+        String signedStr = parameters.get("signed");
+        if (signedStr != null) {
+            signed = Boolean.valueOf(signedStr);
+        }
+
+        String encodingStr = parameters.get("encoding");
+        if (encodingStr != null) {
+            if (encodingStr.equals("pcm")) {
+                encoding = (signed == true ? AudioFormat.Encoding.PCM_SIGNED :
+                            AudioFormat.Encoding.PCM_UNSIGNED);
+            } else if (encodingStr.equals("alaw")) {
+                encoding = AudioFormat.Encoding.ALAW;
+                endian = false;
+            } else if (encodingStr.equals("ulaw")) {
+                encoding = AudioFormat.Encoding.ULAW;
+                endian = false;
+            } else if (encodingStr.equals("gsm")) {
+                /** @todo GSM not supported by AudioFormat */
+                System.err.println("GSM not supported by AudioFormat... review");
+            }
+        }
+
+        String rateStr = parameters.get("rate");
+        if (rateStr != null) {
+            sampleRate = Float.valueOf(rateStr);
+        }
+
+        String bitsStr = parameters.get("bits");
+        if (bitsStr != null) {
+            bits = Integer.valueOf(bitsStr);
+            if (bits == 8) {
+                endian = false;
+            }
+        }
+
+        String channelsStr = parameters.get("channels");
+        if (channelsStr != null) {
+            channels = Integer.valueOf(channelsStr);
+        }
+
+        String endianStr = parameters.get("endian");
+        if (endianStr != null) {
+            if (endianStr.equals("little")) {
+                endian = false;
+            } else if (endianStr.equals("big")) {
+                endian = true;
+            }
+        }
+
+        //Constructs the AudioFormat
+        return new AudioFormat(encoding, sampleRate, bits, channels, bits / 8,
+                               sampleRate, endian);
+    }
+
+    public void setEngineAudioFormat(AudioFormat audioFormat) {
+        engineAudioFormat = audioFormat;
+    }
+
+    public AudioFormat getEngineAudioFormat() {
+        return engineAudioFormat;
+    }
+
+    /**
+     * @todo Insure that this is robust...
+     *
+     * @param is InputStream
+     * @param sourceFormat AudioFormat
+     * @param targetFormat AudioFormat
+     * @return InputStream
+     */
+    private InputStream getConvertedStream(InputStream is,
+                                           AudioFormat sourceFormat,
+                                           AudioFormat targetFormat) {
+        /** @todo Compare more preciselly AudioFormat (not using AudioFormat.matches()) */
+        if (sourceFormat.matches(targetFormat)) {
+            return is;
+        }
+
+        //Describe source stream as an AudioFormat
+        AudioInputStream sourceStream = new AudioInputStream(is, sourceFormat,
+                AudioSystem.NOT_SPECIFIED);
+
+        //Convert endianess
+        sourceStream = convertEndianess(sourceStream, targetFormat);
+
+        //Convert number of channels
+        sourceStream = convertChannels(sourceStream, targetFormat);
+
+        //Convert sample rate
+        sourceStream = convertSampleRate(sourceStream, targetFormat);
+
+        //Convert encoding
+        sourceStream = convertEncoding(sourceStream, targetFormat);
+
+        //Convert sample size
+        sourceStream = convertSampleSize(sourceStream, targetFormat);
+
+        return sourceStream;
+    }
+
+    /**
+     * @todo Insure that this is robust...
+     *
+     * @param os OutputStream
+     * @param engineAudioFormat AudioFormat
+     * @param audioFormat AudioFormat
+     * @return OutputStream
+     */
+    private OutputStream getConvertedStream(final OutputStream os,
+                                            AudioFormat engineAudioFormat,
+                                            AudioFormat targetFormat) {
+        /** @todo Compare more preciselly AudioFormat (not using AudioFormat.matches()) */
+        if (engineAudioFormat.matches(targetFormat)) {
+            return os;
+        }
+
+        try {
+            //Basic Conversion support
+            PipedInputStream pis = new PipedInputStream(16000000);
+            PipedOutputStream pos = new PipedOutputStream(pis);
+
+            //Describe source audio
+            final InputStream is = getConvertedStream(pis, engineAudioFormat,
+                    targetFormat);
+
+            new Thread(new Runnable() {
+                public void run() {
+                    FileOutputStream synt_conv = null;
+                    try {
+                        synt_conv = new FileOutputStream(File.
+                                createTempFile("synth", ".raw", new File(".")));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    int br;
+                    byte[] buffer = new byte[512];
+                    try {
+                        while ((br = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, br);
+                            synt_conv.write(buffer, 0, br);
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    try {
+                        os.close();
+                        is.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }, "AudioCopyer").start();
+
+            //Return write point for synthesizer
+            return pos;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        /** @todo Should never reach this point */
+        return os;
+    }
+
+
+    private AudioInputStream convertEndianess(AudioInputStream ais,
+                                              AudioFormat targetFormat) {
+        if (ais.getFormat().isBigEndian() == targetFormat.isBigEndian()) {
+            return ais;
+        }
+
+        AudioFormat newFormat = new AudioFormat(ais.getFormat().getEncoding(),
+                                                ais.getFormat().getSampleRate(),
+                                                ais.getFormat().
+                                                getSampleSizeInBits(),
+                                                ais.getFormat().getChannels(),
+                                                ais.getFormat().getFrameSize(),
+                                                ais.getFormat().getFrameRate(),
+                                                targetFormat.isBigEndian());
+
+        return AudioSystem.getAudioInputStream(newFormat, ais);
+    }
+
+    private AudioInputStream convertChannels(AudioInputStream ais,
+                                             AudioFormat targetFormat) {
+        if (ais.getFormat().getChannels() == targetFormat.getChannels()) {
+            return ais;
+        }
+        AudioFormat newFormat = new AudioFormat(ais.getFormat().getEncoding(),
+                                                ais.getFormat().getSampleRate(),
+                                                ais.getFormat().
+                                                getSampleSizeInBits(),
+                                                targetFormat.getChannels(),
+                                                ais.getFormat().getFrameSize(),
+                                                ais.getFormat().getFrameRate(),
+                                                ais.getFormat().isBigEndian());
+
+        return AudioSystem.getAudioInputStream(newFormat, ais);
+
+    }
+
+    private AudioInputStream convertSampleRate(AudioInputStream ais,
+                                               AudioFormat targetFormat) {
+        if (ais.getFormat().getSampleRate() == targetFormat.getSampleRate()) {
+            return ais;
+        }
+        AudioFormat newFormat = new AudioFormat(ais.getFormat().getEncoding(),
+                                                targetFormat.getSampleRate(),
+                                                ais.getFormat().
+                                                getSampleSizeInBits(),
+                                                ais.getFormat().getChannels(),
+                                                ais.getFormat().getFrameSize(),
+                                                targetFormat.getFrameRate(),
+                                                ais.getFormat().isBigEndian());
+
+        return AudioSystem.getAudioInputStream(newFormat, ais);
+    }
+
+    private AudioInputStream convertEncoding(AudioInputStream ais,
+                                             AudioFormat targetFormat) {
+        if (ais.getFormat().getEncoding() == targetFormat.getEncoding()) {
+            return ais;
+        }
+        AudioFormat newFormat = new AudioFormat(targetFormat.getEncoding(),
+                                                ais.getFormat().getSampleRate(),
+                                                targetFormat.
+                                                getSampleSizeInBits(),
+                                                ais.getFormat().getChannels(),
+                                                targetFormat.getFrameSize(),
+                                                ais.getFormat().getFrameRate(),
+                                                ais.getFormat().isBigEndian());
+
+        return AudioSystem.getAudioInputStream(newFormat, ais);
+    }
+
+
+    private AudioInputStream convertSampleSize(AudioInputStream ais,
+                                               AudioFormat targetFormat) {
+        if (ais.getFormat().getSampleSizeInBits() ==
+            targetFormat.getSampleSizeInBits()) {
+            return ais;
+        }
+        AudioFormat newFormat = new AudioFormat(ais.getFormat().getEncoding(),
+                                                ais.getFormat().getSampleRate(),
+                                                targetFormat.
+                                                getSampleSizeInBits(),
+                                                ais.getFormat().getChannels(),
+                                                ais.getFormat().getFrameSize(),
+                                                ais.getFormat().getFrameRate(),
+                                                ais.getFormat().isBigEndian());
+
+        return AudioSystem.getAudioInputStream(newFormat, ais);
     }
 
 
