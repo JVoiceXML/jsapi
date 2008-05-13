d35512ab807bf5145d1e970819156d88839467ce
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2008-05-13 07:32:55 +0000

    Added demo for speech synthesis.

---
+++ demo/HelloWorld/src/org/jvoicexml/jsapi2/demo/helloworld/HelloWorld/HelloWorldDemo.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.jvoicexml.jsapi2.demo.helloworld.HelloWorld;
+
+import javax.speech.EngineManager;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerMode;
+
+/**
+ * @author DS01191
+ *
+ */
+public class HelloWorldDemo {
+    public static void main(String args[]) {
+        try {
+            // Create a synthesizer for the default Locale
+            Synthesizer synth = (Synthesizer)
+                EngineManager.createEngine(SynthesizerMode.DEFAULT);
+            
+            // Get it ready to speak
+            synth.allocate();
+
+            // Speak the "hello world" string
+            synth.speak("Hello, world!", null);
+
+            // Clean up - includes waiting for the queue to empty
+            synth.deallocate();
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
