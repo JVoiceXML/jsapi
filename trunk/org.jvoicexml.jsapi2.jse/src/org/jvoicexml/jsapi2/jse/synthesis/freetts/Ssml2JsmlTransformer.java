dd5881289bf8df83413bca49187fe066a8eda357
davidjrodrigues <davidjrodrigues@8d230bc6-492e-0410-ad60-ad825706fda1>
2008-07-11 14:27:20 +0000

    allows to convert SSML to JSML (commit missing classe)

---
+++ trunk/org.jvoicexml.jsapi2.jse/src/org/jvoicexml/jsapi2/jse/synthesis/freetts/Ssml2JsmlTransformer.java
@@ -0,0 +1,90 @@
+package org.jvoicexml.jsapi2.jse.synthesis.freetts;
+
+import org.w3c.dom.Document;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import javax.xml.transform.TransformerFactory;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+
+import org.xml.sax.SAXException;
+
+public class Ssml2JsmlTransformer {
+
+    private TransformerFactory tfactory;
+    private DOMSource xmlDomSource;
+    private DocumentBuilder domBuilder;
+    private DOMResult xmlDomResult;
+    private DocumentBuilderFactory domFactory;
+    private Transformer serializer = null;
+
+    private Templates template;
+
+    public Ssml2JsmlTransformer() {
+        tfactory = TransformerFactory.newInstance();
+        xmlDomSource = null;
+        domBuilder = null;
+        domFactory = DocumentBuilderFactory.newInstance();
+        try {
+            domBuilder = domFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+        }
+
+        /** create a template from a xsl file */
+        try {
+            template = tfactory.newTemplates(new StreamSource(new
+                    FileInputStream("ssml2jsml.xsl")));
+        } catch (FileNotFoundException ex2) {
+            ex2.printStackTrace();
+        } catch (TransformerConfigurationException ex2) {
+            ex2.printStackTrace();
+        }
+
+        try {
+            serializer = template.newTransformer();
+        } catch (TransformerConfigurationException ex3) {
+            ex3.printStackTrace();
+        }
+    }
+
+    public Document transform(String ssml) {
+        try {
+            final ByteArrayInputStream bais = new ByteArrayInputStream(ssml.
+                    getBytes());
+            xmlDomSource = new DOMSource(domBuilder.parse(bais));
+
+        } catch (IOException ex1) {
+            ex1.printStackTrace();
+        } catch (SAXException ex1) {
+            ex1.printStackTrace();
+        }
+
+        Document resDocument = domBuilder.newDocument();
+        xmlDomResult = new DOMResult(resDocument);
+
+        /** transform to a Document */
+        try {
+            serializer.transform(xmlDomSource, xmlDomResult);
+        } catch (TransformerException ex4) {
+            ex4.printStackTrace();
+        }
+
+        return resDocument;
+    }
+}
