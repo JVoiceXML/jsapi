f5e8c52eba1af1c84a663a8dde4cd7fc1d4aa030
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2008-09-10 07:51:13 +0000

    Added initial setup.

--- trunk/org.jvoicexml.jsapi2.jse/demo/HelloWorld/src/org/jvoicexml/jsapi2/demo/helloworld/HelloWorld/HelloWorldDemo.java
+++ trunk/org.jvoicexml.jsapi2.jse/demo/HelloWorld/src/org/jvoicexml/jsapi2/demo/helloworld/HelloWorld/HelloWorldDemo.java
@@ -3,6 +3,11 @@
  */
 package org.jvoicexml.jsapi2.demo.helloworld.HelloWorld;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.speech.EngineManager;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerMode;
@@ -11,27 +16,38 @@ import org.jvoicexml.jsapi2.jse.synthesis.freetts.FreeTTSEngineListFactory;
 
 /**
  * @author DS01191
- *
+ * 
  */
 public class HelloWorldDemo {
     public static void main(String args[]) {
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        Logger.getLogger("com.sun").addHandler(handler);
+        Logger.getLogger("com.sun").setLevel(Level.ALL);
         try {
-        	EngineManager.registerEngineListFactory(
-        			FreeTTSEngineListFactory.class.getName());
+            EngineManager
+                    .registerEngineListFactory(FreeTTSEngineListFactory.class
+                            .getName());
+            System.setProperty("javax.speech.supports.audio.management",
+                    Boolean.TRUE.toString());
             // Create a synthesizer for the default Locale
-            Synthesizer synth = (Synthesizer)
-                EngineManager.createEngine(SynthesizerMode.DEFAULT);
-            
+            Synthesizer synth = (Synthesizer) EngineManager
+                    .createEngine(SynthesizerMode.DEFAULT);
+
             // Get it ready to speak
             synth.allocate();
 
             // Speak the "hello world" string
+            System.out.println("Speaking 'Hello, world!'...");
             synth.speak("Hello, world!", null);
+            synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
+//            Thread.sleep(3000);
+            System.out.println("done.");
 
             // Clean up - includes waiting for the queue to empty
             synth.deallocate();
-        } 
-        catch (Exception e) {
+            System.exit(0);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
