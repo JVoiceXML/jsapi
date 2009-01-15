eeca109345bc8ff54cd717be2d1806d7f1d9b069
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2009-01-15 17:47:00 +0000

    Outputting the result.

--- trunk/org.jvoicexml.jsapi2.jse/demo/InputDemo/src/org/jvoicexml/jsapi2/demo/inputdemo/InputDemo.java
+++ trunk/org.jvoicexml.jsapi2.jse/demo/InputDemo/src/org/jvoicexml/jsapi2/demo/inputdemo/InputDemo.java
@@ -9,12 +9,15 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.speech.Engine;
 import javax.speech.EngineManager;
 import javax.speech.recognition.GrammarManager;
 import javax.speech.recognition.Recognizer;
 import javax.speech.recognition.RecognizerMode;
+import javax.speech.recognition.Result;
 import javax.speech.recognition.ResultEvent;
 import javax.speech.recognition.ResultListener;
+import javax.speech.recognition.ResultToken;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerMode;
 
@@ -26,6 +29,39 @@ import org.jvoicexml.jsapi2.jse.synthesis.freetts.FreeTTSEngineListFactory;
  *
  */
 public class InputDemo implements ResultListener {
+    /** The synthesizer to use. */
+    private Synthesizer synthesizer;
+
+    /** The recognizer to use. */
+    private Recognizer recognizer;
+
+    public void run() throws Exception {
+        // Create a synthesizer for the default Locale.
+        synthesizer = (Synthesizer)
+            EngineManager.createEngine(SynthesizerMode.DEFAULT);
+        // Create a recognizer for the default Locale.
+        recognizer = (Recognizer) EngineManager.createEngine(
+                RecognizerMode.DEFAULT);
+
+        // Get it ready to speak
+        synthesizer.allocate();
+
+
+        recognizer.allocate();
+        recognizer.addResultListener(this);
+
+        GrammarManager grammarManager = recognizer.getGrammarManager();
+        InputStream in = InputDemo.class.getResourceAsStream("hello.xml");
+        grammarManager.loadGrammar("grammar:greeting", null, in, "UTF-8");
+
+        recognizer.requestFocus();
+        recognizer.resume();
+        // Tell the user what to do as soon as the recognizer is ready.
+        recognizer.waitEngineState(Engine.RESUMED);
+        // Speak the intro string
+        synthesizer.speak("Please say something", null);
+        System.out.println("Please say something...");
+    }
 
     /**
      * @param args
@@ -46,42 +82,34 @@ public class InputDemo implements ResultListener {
                     Boolean.TRUE.toString());
             System.setProperty("javax.speech.supports.audio.capture",
                     Boolean.TRUE.toString());
-            // Create a synthesizer for the default Locale
-            Synthesizer synth = (Synthesizer) EngineManager
-                    .createEngine(SynthesizerMode.DEFAULT);
 
             EngineManager.registerEngineListFactory(
                     SphinxEngineListFactory.class.getName());
 
-            // Get it ready to speak
-            synth.allocate();
-
-            Recognizer recognizer = (Recognizer) EngineManager.createEngine(
-                    RecognizerMode.DEFAULT);
-
-            recognizer.allocate();
-            recognizer.addResultListener(demo);
-
-            GrammarManager grammarManager = recognizer.getGrammarManager();
-            InputStream in = InputDemo.class.getResourceAsStream("hello.xml");
-            grammarManager.loadGrammar("grammar:greeting", null, in, "UTF-8");
-            // Speak the "hello world" string
-            synth.speak("Please say something", null);
-            synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
-            System.out.println("Please say something...");
-
-            recognizer.requestFocus();
-            recognizer.resume();
-
+            demo.run();
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(-1);
         }
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void resultUpdate(ResultEvent e) {
-        System.out.println(e);
+    public void resultUpdate(final ResultEvent event) {
+        if (event.getId() == ResultEvent.RESULT_ACCEPTED) {
+            Result result = (Result) (event.getSource());
+            ResultToken tokens[] = result.getBestTokens();
+
+            for (int i = 0; i < tokens.length; i++) {
+                System.out.print(tokens[i].getText() + " ");
+            }
+            System.out.println();
+        } else {
+            synthesizer.speak("I did not undersatnd what you said", null);
+        }
     }
 
 }
