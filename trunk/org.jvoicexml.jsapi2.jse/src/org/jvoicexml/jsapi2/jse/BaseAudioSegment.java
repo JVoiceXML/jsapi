64da9898487edfaf0d25a8e4d1a76ed7af66a088
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2009-02-13 20:26:08 +0000

    Enhanced javadoc, some code cleanup.

--- trunk/org.jvoicexml.jsapi2.jse/src/org/jvoicexml/jsapi2/jse/BaseAudioSegment.java
+++ trunk/org.jvoicexml.jsapi2.jse/src/org/jvoicexml/jsapi2/jse/BaseAudioSegment.java
@@ -1,38 +1,77 @@
+/*
+ * File:    $HeadURL: $
+ * Version: $LastChangedRevision: $
+ * Date:    $LastChangedDate $
+ * Author:  $LastChangedBy: lyncher $
+ *
+ * JSAPI - An base implementation for JSR 113.
+ *
+ * Copyright (C) 2007-2009 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ */
+
 package org.jvoicexml.jsapi2.jse;
 
-import javax.speech.AudioSegment;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.speech.AudioSegment;
 
 /**
- * <p>Title: JSAPI 2.0</p>
- *
- * <p>Description: An independent reference implementation of JSR 113</p>
- *
- * <p>Copyright: Copyright (c) 2007</p>
- *
- * <p>Company: JVoiceXML group - http://jvoicexml.sourceforge.net</p>
- *
- * @author not attributable
- * @version 1.0
+ * Basic implementation of an {@link AudioSegment}.
+ * <p>
+ * The association of audio data to a representation is based on an
+ * {@link InputStream}.
+ * </p>
+ * @author Renato Cassaca
+ * @version $Revision: 1370 $
+
  */
 public class BaseAudioSegment extends AudioSegment {
-
+    /** The audio data. */
     private final InputStream is;
 
-    public BaseAudioSegment(String locator, String markupText) {
+    /**
+     * Constructs a new object without an input stream. This means that
+     * there is no associated audio data.
+     * @param locator a non-null media locator description.
+     * @param markupText the alternate markup text
+     */
+    public BaseAudioSegment(final String locator, final String markupText) {
         super(locator, markupText);
         is = null;
     }
 
-    public BaseAudioSegment(String locator, String markupText, InputStream is) {
+    /**
+     * Constructs a new object with the given input stream.
+     * @param locator a non-null media locator description.
+     * @param markupText the alternate markup text
+     * @param input the input stream for the audio data.
+     */
+    public BaseAudioSegment(final String locator, final String markupText,
+            final InputStream input) {
         super(locator, markupText);
-        this.is = is;
+        is = input;
     }
 
-    public InputStream openInputStream()  throws IOException, SecurityException{
+    /**
+     * Opens the input stream. If an input stream is given when creating this
+     * object, this one is returned, otherwise this implementation tries
+     * to open a stream to the given media locator.
+     * @exception IOException
+     *            error opening the input stream.
+     * @return the input stream with the audio data.
+     */
+    public InputStream openInputStream() throws IOException {
+        if (!isGettable()) {
+            throw new SecurityException(
+                    "The platform does not allow to access the input stream!");
+        }
         if (is == null) {
-            return super.openInputStream();
+            final String locator = getMediaLocator();
+            final URL url = new URL(locator);
+            return url.openStream();
         } else {
             return is;
         }
