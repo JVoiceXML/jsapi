fa92ea1d27e6cd507e3ca0693e4d9da238cef942
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2010-03-24 09:26:38 +0000

    Refactoring: Moved SynthQueue to an inner class.

--- trunk/org.jvoicexml.jsapi2/src/org/jvoicexml/jsapi2/synthesis/BaseSynthesizer.java
+++ trunk/org.jvoicexml.jsapi2/src/org/jvoicexml/jsapi2/synthesis/BaseSynthesizer.java
@@ -26,21 +26,7 @@ import org.jvoicexml.jsapi2.BaseEngineProperties;
 
 
 /**
- * <p>
- * Title: JSAPI 2.0
- * </p>
- *
- * <p>
- * Description: An independent reference implementation of JSR 113
- * </p>
- *
- * <p>
- * Copyright: Copyright (c) 2007
- * </p>
- *
- * <p>
- * Company: JVoiceXML group - http://jvoicexml.sourceforge.net
- * </p>
+ * Basic synthesizer functions.
  *
  * @author Renato Cassaca
  * @author Dirk Schnelle-Walka
@@ -90,7 +76,6 @@ public abstract class BaseSynthesizer extends BaseEngine
      * @param oldState long
      * @param newState long
      * @param eventType int
-     * @todo Implement this org.jvoicexml.jsapi2.jse.j2se.BaseEngine method
      */
     public void postEngineEvent(long oldState, long newState, int eventType) {
         final SynthesizerEvent event = new SynthesizerEvent(this, eventType,
