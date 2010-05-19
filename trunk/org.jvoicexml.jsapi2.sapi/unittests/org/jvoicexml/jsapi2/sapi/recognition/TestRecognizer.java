8047627348b8ea3b3ff6216a9fc8dfa23c76f2e2
josarn <josarn@8d230bc6-492e-0410-ad60-ad825706fda1>
2010-05-19 15:41:18 +0000

    added new method tests

--- trunk/org.jvoicexml.jsapi2.sapi/unittests/org/jvoicexml/jsapi2/sapi/recognition/TestRecognizer.java
+++ trunk/org.jvoicexml.jsapi2.sapi/unittests/org/jvoicexml/jsapi2/sapi/recognition/TestRecognizer.java
@@ -1,12 +1,35 @@
 package org.jvoicexml.jsapi2.sapi.recognition;
 
 import javax.speech.AudioException;
+import javax.speech.Engine;
 import javax.speech.EngineException;
 import javax.speech.EngineStateException;
 
 import org.jvoicexml.jsapi2.sapi.recognition.SapiRecognizer;
+import org.jvoicexml.jsapi2.sapi.synthesis.SapiSynthesizer;
 
+/**
+ * Test cases for {@link SapiRecognizer}.
+ * <p>
+ * Run this unit test with the VM option:
+ * <code>-Djava.library.path=cpp/Jsapi2SapiBridge/Debug</code>.
+ * </p>
+ * @author Dirk Schnelle-Walka
+ * @author Josua Arndt
+ *
+ */
 public class TestRecognizer {
+    
+//    public void testRecognition() throws Exception {
+//        
+//        SapiRecognizer recognizer = new SapiRecognizer();
+//        recognizer.allocate();
+//        recognizer.waitEngineState(Engine.ALLOCATED);
+//        
+//        recognizer.setGrammar("Licht.xml");       
+//        Thread.sleep(6000);
+//        recognizer.deallocate();
+//    }
 	
     public static void main(String[] args) throws InterruptedException 
     { 
@@ -31,11 +54,22 @@ public class TestRecognizer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-		recognizer.setGrammar("Licht.xml");
-
-		
+				 	
+		recognizer.setGrammar("Licht.xml");		
+		System.out.println( "Load grammar:\t\tokay");
 		Thread.sleep(20);
+		
+		recognizer.startRecognition();		
+		System.out.println( "Start:\t\tokay");
+		Thread.sleep(2000);
+		
+		recognizer.pause();
+		System.out.println( "Pause:\t\tokay");
+		Thread.sleep(200);
+		
+		recognizer.resume();	
+		System.out.println( "resume:\t\tokay");
+		Thread.sleep(400);
 		
 		recognizer.handleDeallocate();
 		System.out.println( "Deallocate:\t\tokay");
