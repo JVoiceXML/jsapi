65dbc169a09a91da1d7b4bbe18220351f8f3c254
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2010-06-17 07:12:14 +0000

    Added missing source files.

---
+++ trunk/org.jvoicexml.jsapi2.sapi/cpp/Jsapi2SapiBridge/Jsapi2SapiBridge/SapiEngineListFactory.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+#include "org_jvoicexml_jsapi2_sapi_SapiEngineListFactory.h"
+#include "Synthesizer.h"
+
+JNIEXPORT jobjectArray JNICALL Java_org_jvoicexml_jsapi2_sapi_SapiEngineListFactory_sapiGetVoices
+  (JNIEnv *env, jobject obj)
+{
+    Voice* voices = NULL;
+    ULONG num;
+    Synthesizer::ListVoices(voices, num);
+
+    jclass clazz = env->FindClass("javax/speech/synthesis/Voice");
+    if (clazz == NULL)
+    {
+        char* msg = "Unable to create javax/speech/synthesis/Voice!";
+        jclass exception = env->FindClass("java/lang/NullPointerException");
+        if (exception == 0) /* Unable to find the new exception class, give up. */
+        {
+            std::cerr << msg << std::endl;
+            return NULL;
+        }
+        env->ThrowNew(exception, msg);
+        return NULL;
+    }
+    jmethodID constructor = env->GetMethodID(clazz, "<init>",
+        "(Ljavax/speech/SpeechLocale;Ljava/lang/String;III)V");
+    if (constructor == NULL)
+    {
+        char* msg = "Constructor for javax/speech/synthesis/Voice not found!";
+        jclass exception = env->FindClass("java/lang/NullPointerException");
+        if (exception == 0) /* Unable to find the new exception class, give up. */
+        {
+            std::cerr << msg << std::endl;
+            return NULL;
+        }
+        env->ThrowNew(exception, msg);
+        return NULL;
+    }
+
+    jobjectArray jvoices = env->NewObjectArray(num, clazz, NULL);
+    if (jvoices == NULL)
+    {
+        char* msg = "Error creating the voices array!";
+        jclass exception = env->FindClass("java/lang/NullPointerException");
+        if (exception == 0) /* Unable to find the new exception class, give up. */
+        {
+            std::cerr << msg << std::endl;
+            return NULL;
+        }
+        env->ThrowNew(exception, msg);
+        return NULL;
+    }
+    for (ULONG i=0; i<num; i++)
+    {
+        WCHAR* voiceName = voices[i].GetName();
+        jstring name = env->NewString((jchar*)voiceName, wcslen(voiceName));
+        jobject voice = env->NewObject(clazz, constructor, NULL, name, -1, -1, -1);
+        env->SetObjectArrayElement(jvoices, i, voice);
+    }
+
+    if (voices != NULL)
+    {
+        delete[] voices;
+    }
+    return jvoices;
+}
+
