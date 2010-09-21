019021014974335ee1e5e228c14667641faf594a
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2010-09-21 18:49:56 +0000

    Avoided german comments.

--- trunk/org.jvoicexml.jsapi2.sapi/cpp/Jsapi2SapiBridge/include/stdafx.h
+++ trunk/org.jvoicexml.jsapi2.sapi/cpp/Jsapi2SapiBridge/include/stdafx.h
@@ -1,20 +1,13 @@
-// stdafx.h : Includedatei für Standardsystem-Includedateien
-// oder häufig verwendete projektspezifische Includedateien,
-// die nur in unregelmäßigen Abständen geändert werden.
-//
-
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Selten verwendete Teile der Windows-Header nicht einbinden.
+#define WIN32_LEAN_AND_MEAN
 
-// Windows-Headerdateien:
+// Windows-Headerfiles:
 #include <sapi.h>
 #include <sphelper.h>
 #include "resource.h"
 
-
-// TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
 
 #include <iostream>
 void GetErrorMessage(char* buffer, size_t size, const char* text, HRESULT hr);
