edbfb6460b53583fddb9b91292d6731474b727ad
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2011-08-17 17:20:48 +0000

    Using Java Logger instead of System.out.println.

--- org.jvoicexml.jsapi2.sapi/src/org/jvoicexml/jsapi2/sapi/recognition/SapiRecognitionThread.java
+++ org.jvoicexml.jsapi2.sapi/src/org/jvoicexml/jsapi2/sapi/recognition/SapiRecognitionThread.java
@@ -1,6 +1,8 @@
 package org.jvoicexml.jsapi2.sapi.recognition;
 
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Perform the recognition in an own java process.
@@ -9,6 +11,10 @@ import java.util.Arrays;
  *
  */
 final class SapiRecognitionThread extends Thread {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+        Logger.getLogger(SapiRecognitionThread.class.getName());
+
     /** The calling SapiRecognizer.  **/
     private SapiRecognizer recognizer;
    
@@ -27,12 +33,21 @@ final class SapiRecognitionThread extends Thread {
     public void run() {
         final long handle = recognizer.getRecognizerHandle();
         //start recognition and get the recognition result
-        System.out.println("--RecognitionThread: Starting recognition process");
-        String tmp[] = recognizer.sapiRecognize(handle);
-        System.out.println("++RecognitionThread: Recognitionprocess ended");
-        
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Starting recognition process");
+        }
+        final String tmp[] = recognizer.sapiRecognize(handle);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Recognitionprocess ended");
+        }
+
         //copy the result into a local variable and notify the recognizer
-        String result[] = (tmp==null ? null : Arrays.copyOf(tmp, 2));
+        final String result[];
+        if (tmp == null) {
+            result = null;
+        } else {
+            result = Arrays.copyOf(tmp, 2);
+        }
         recognizer.reportResult(result);
     }
 
@@ -40,9 +55,13 @@ final class SapiRecognitionThread extends Thread {
      * Stops the recognition process.
      */
     public void stopRecognition() {
-        System.out.println("--Recognizer: Issuing Interrupt to Recognition-Thread");
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Issuing Interrupt to Recognition-Thread...");
+        }
         interrupt();
-        System.out.println("++Recognizer: Issuing Interrupt to Recognition-Thread -- DONE!");
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("...issued Interrupt to Recognition-Thread");
+        }
         final long handle = recognizer.getRecognizerHandle();
         recognizer.sapiAbortRecognition(handle);
     }
