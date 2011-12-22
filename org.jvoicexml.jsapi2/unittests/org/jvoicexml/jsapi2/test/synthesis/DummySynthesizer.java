520970508cfc79ecca3a22bd497a5ebe3471fca9
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2011-12-22 22:15:30 +0000

    started to work on a recognizer state test

--- org.jvoicexml.jsapi2/unittests/org/jvoicexml/jsapi2/test/synthesis/DummySynthesizer.java
+++ org.jvoicexml.jsapi2/unittests/org/jvoicexml/jsapi2/test/synthesis/DummySynthesizer.java
@@ -1,4 +1,5 @@
 package org.jvoicexml.jsapi2.test.synthesis;
+
 import javax.speech.AudioException;
 import javax.speech.AudioManager;
 import javax.speech.EngineException;
@@ -13,63 +14,102 @@ import org.jvoicexml.jsapi2.test.DummyAudioManager;
 import org.jvoicexml.jsapi2.test.DummySpeechEventExecutor;
 
 /**
- * 
- */
-
-/**
+ * Dummy implementation of a {@link javax.spech.synthesis.Synthesizer}
+ * for test purposes.
  * @author Dirk Schnelle-Walka
- *
  */
 public class DummySynthesizer extends BaseSynthesizer {
 
+    /**
+     * {@inheritDoc}
+     */
     protected Speakable getSpeakable(String text) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void handleAllocate() throws EngineStateException,
             EngineException, AudioException, SecurityException {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected boolean handleCancel() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected boolean handleCancel(int id) {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected boolean handleCancelAll() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void handleDeallocate() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void handlePause() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected boolean handleResume() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void handleSpeak(int id, String item) {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void handleSpeak(int id, Speakable item) {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected AudioManager createAudioManager() {
         return new DummyAudioManager();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected SpeechEventExecutor createSpeechEventExecutor() {
         return new DummySpeechEventExecutor();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected VocabularyManager createVocabularyManager() {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected EnginePropertyChangeRequestListener getChangeRequestListener() {
         return null;
     }
