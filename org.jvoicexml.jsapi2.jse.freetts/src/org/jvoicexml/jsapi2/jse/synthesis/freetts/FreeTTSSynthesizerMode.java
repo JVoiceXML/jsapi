e9d5d42accf1b655aa2b939349a819675852b362
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2012-01-20 11:49:48 +0000

    Style corrections.

--- org.jvoicexml.jsapi2.jse.freetts/src/org/jvoicexml/jsapi2/jse/synthesis/freetts/FreeTTSSynthesizerMode.java
+++ org.jvoicexml.jsapi2.jse.freetts/src/org/jvoicexml/jsapi2/jse/synthesis/freetts/FreeTTSSynthesizerMode.java
@@ -1,21 +1,36 @@
+/*
+ * File:    $HeadURL: https://svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/Application.java$
+ * Version: $LastChangedRevision: 68 $
+ * Date:    $LastChangedDate $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JSAPI - An independent reference implementation of JSR 113.
+ *
+ * Copyright (C) 2007-2012 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package org.jvoicexml.jsapi2.jse.synthesis.freetts;
 
 import java.util.Locale;
-
 import javax.speech.Engine;
 import javax.speech.EngineException;
 import javax.speech.spi.EngineFactory;
 import javax.speech.synthesis.SynthesizerMode;
-
-
-/**
- * Copyright 2003 Sun Microsystems, Inc.
- *
- * See the file "license.terms" for information on usage and
- * redistribution of this file, and for a DISCLAIMER OF ALL
- * WARRANTIES.
- */
-
 
 
 /**
@@ -57,6 +72,7 @@ public class FreeTTSSynthesizerMode extends SynthesizerMode implements
      * @throws SecurityException if the caller does not have
      * 		permission to use the speech engine
      */
+    @Override
     public Engine createEngine() throws IllegalArgumentException,
             EngineException, SecurityException {
         return new FreeTTSSynthesizer(this);
