2596f78ebd79047a8164ff0717dfff4a8c74f9ce
schnelle <schnelle@8d230bc6-492e-0410-ad60-ad825706fda1>
2013-09-24 07:30:20 +0000

    Initial import of the security check project for javax.speech.

---
+++ trunk/javax.speech.security/src/javax/speech/JavaSpeechSecurity.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package javax.speech;
+
+
+/**
+ * A <code>null</code> implementation of a security check. This class only
+ * exists to create a hook for implementations of security checks for the
+ * Java Speech API. Since CLDC 1.0 does not know anything about
+ * {@link java.security.Permission} this approach is necessary. 
+ * This class should never be in the classpath of a
+ * runtime.
+ * <p>
+ * Implementations are requested to keep the signature of this method and
+ * perform a security check with the help of a class
+ * <code>SpeechPermissiony</code> that needs to be written and integrated into
+ * the implementation.
+ * <pre>
+ * package javax.speech;
+ * 
+ * import javax.security.Permission;
+ * 
+ * public class SpeechPermission extends Permission {
+ *    ...
+ * }
+ * </pre>
+ * </p>
+ * 
+ * 
+ * @author Dirk Schnelle-Walka
+ * 
+ */
+public class JavaSpeechSecurity {
+    /**
+     * Must be overriden by implementations.
+     * @param name permission to check for
+     * @throws SecurityException
+     *         always
+     */
+    public static void checkPermission(final String name) throws SecurityException {
+        throw new SecurityException("null implementation rejected '" + name
+                + "'");
+    }
+}
